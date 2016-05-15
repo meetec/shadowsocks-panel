2b4c43924a3e6cb8333ae571dc8274818af75658
meetec <Apple@Apples-MacBook-Pro.local>
2016-05-15 18:18:16 +0200

    1.15 sync to 1.16

--- Library/Controller/Node.php
+++ Library/Controller/Node.php
@@ -41,69 +41,14 @@ class Node
         }
         $info = self::nodeDetail($node->server, $user->port, $user->sspwd, $method, $node->name);
         if (self::verifyPlan($user->plan, $node->type)) {
-            $result = array('error' => 0, 'message' => '获取成功', 'info' => $info, 'node' => $node);
+            $result = array('error' => 0, 'message' => 'Success', 'info' => $info, 'node' => $node);
         } else {
-<<<<<<< HEAD
-            Message::show('You are not VIP, Unable to use the advanced node!', 'member/node');
-=======
-            $result = array('error' => -1, 'message' => '你不是 VIP, 无法使用高级节点！');
->>>>>>> sendya/master
+            $result = array('error' => -1, 'message' => 'You are not VIP, you can not use the advanced node!');
         }
         return $result;
     }
 
-<<<<<<< HEAD
-    public function json()
-    {
-        $id = trim($_REQUEST['id']);
-        $user = User::getUserByUserId(User::getCurrent()->uid);
-        $node = NodeModel::getNodeById($id);
-        $method = $node->method;
-        if($node->custom_method == 1 && $user->method != '' && $user->method != null) {
-            $method = $user->method;
-        }
-        $info = self::nodeJson($node->server, $user->port, $user->sspwd, $method, $node->name);
-        if (self::verifyPlan($user->plan, $node->type)) {
-            Template::putContext('info', $info);
-            Template::putContext('node', $node);
-            Template::setView('node/Json');
-        } else {
-            Message::show('You are not VIP, Unable to use the advanced node!', 'member/node');
-        }
-    }
-
-    /**
-     * Export node list
-     */
-    public function jsonList()
-    {
-        $user = User::getUserByUserId(User::getCurrent()->uid);
-        $nodeList = NodeModel::getNodeArray();
-        $info = "";
-
-        foreach ($nodeList as $node) {
-            $method = $node->method;
-            if($node->custom_method == 1 && $user->method != '' && $user->method != null) {
-                $method = $user->method;
-            }
-            if (self::verifyPlan($user->plan, $node->type)) {
-                $info .= self::nodeJson($node->server, $user->port, $user->sspwd, $method, $node->name) . ",";
-            }
-        }
-        Template::putContext('info', $info);
-        Template::setView('node/JsonAll');
-    }
-
-
-    private static function nodeJson($server, $server_port, $password, $method, $name)
-    {
-        return '{"server":"' . $server . '","server_port":' . $server_port . ',"local_port":1080,' . '"password":"' . $password . '","timeout":600,' . '"method":"' . $method . '", "remarks": "' . $name . '"}';
-    }
-
-    private static function nodeQr($server, $server_port, $password, $method)
-=======
     private static function nodeDetail($server, $server_port, $password, $method, $name)
->>>>>>> sendya/master
     {
         $ssurl = $method . ":" . $password . "@" . $server . ":" . $server_port;
         $ssurl = "ss://" . base64_encode($ssurl);
